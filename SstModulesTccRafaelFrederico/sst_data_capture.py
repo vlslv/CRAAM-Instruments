86d29992defb81e789d5fddea4e6faddc5c5906a
Rabuiat <rabuiat@hotmail.com>
2018-05-20 02:03:31 -0300

    Feature: Data capture
    Add pretty print when data capture module is finishing

--- SstModulesTccRafaelFrederico/sst_data_capture.py
+++ SstModulesTccRafaelFrederico/sst_data_capture.py
@@ -17,7 +17,7 @@ def extract_ring_list_values(sst_type, sst_date, sst_time, ring_list, clock):
         try:
             start_time = int(round(t.time() * 1000))
 
-            if((("end" in ring_list) and (ring_list.index("end") == index)) or (count_errors == 10)):
+            if(("end" in ring_list) and (ring_list.index("end") == index) or (count_errors == 10)):
                 print(colored("Stopping data capture...\n", "green", attrs = ["bold"]))
                 break
 
@@ -59,6 +59,7 @@ def extract_ring_list_values(sst_type, sst_date, sst_time, ring_list, clock):
                 index += 1
 
             else:
+                print(colored(f"No more data at line: {index} ", "green",  attrs = ["bold"]))
                 count_errors += 1
 
 
